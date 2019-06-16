2bb4277073c057910abd055863b45af975954d6c
Brendan Curry <Brendanstcurry@gmail.com>
2019-06-16 18:29:00 -0500

    scanning

--- src/main/java/org/stormrealms/stormstats/configuration/StormStatConfiguration.java
+++ src/main/java/org/stormrealms/stormstats/configuration/StormStatConfiguration.java
@@ -4,11 +4,15 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.stormrealms.stormstats.model.RPGPlayer;
 
 @Configuration
+@ComponentScan
+@EntityScan
 public class StormStatConfiguration {
 	@Bean(name = "player-cache")
 	public Map<UUID, RPGPlayer> playerCache() {
