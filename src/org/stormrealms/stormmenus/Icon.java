1a1c167f8a01ca9e268e89b83a839bf75f30d262
Brendan Curry <Brendanstcurry@gmail.com>
2020-07-05 18:11:20 -0500

    add placeholders

--- src/org/stormrealms/stormmenus/Icon.java
+++ src/org/stormrealms/stormmenus/Icon.java
@@ -1,6 +1,7 @@
 package org.stormrealms.stormmenus;
 
 import java.util.List;
+import java.util.function.Function;
 
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
@@ -27,6 +28,30 @@ public class Icon {
 		meta.setLore(lore);
 		i.setItemMeta(meta);
 		return i;
+	}
+
+	@SuppressWarnings("deprecation")
+	public ItemStack build(Function<String, String>... replacements) {
+		ItemStack i = new ItemStack(material, 1, (short) dataValue);
+		ItemMeta meta = i.getItemMeta();
+		String newDisplay = displayName;
+		for (int y = 0; y < replacements.length; y++)
+			newDisplay = replacements[y].apply(newDisplay);
+		meta.setDisplayName(newDisplay);
+		meta.setLore(replaceAllLore(lore, replacements));
+		i.setItemMeta(meta);
+		return i;
+	}
+
+	private List<String> replaceAllLore(List<String> lore, Function<String, String>[] replacements) {
+		for (int x = 0; x < lore.size(); x++) {
+			String ogString = lore.get(x);
+			for (int y = 0; y < replacements.length; y++) {
+				ogString = replacements[y].apply(ogString);
+			}
+			lore.set(x, ogString);
+		}
+		return lore;
 	}
 
 	public int getIndex() {
