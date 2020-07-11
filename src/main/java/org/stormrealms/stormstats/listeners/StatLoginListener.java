1e32eb631b7b432f6405bda2a19c9580fa2710c2
Brendan Curry <Brendanstcurry@gmail.com>
2020-07-11 01:40:59 -0500

    commit

--- src/main/java/org/stormrealms/stormstats/listeners/StatLoginListener.java
+++ src/main/java/org/stormrealms/stormstats/listeners/StatLoginListener.java
@@ -2,21 +2,12 @@ package org.stormrealms.stormstats.listeners;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.stereotype.Component;
-import org.stormrealms.stormcore.StormCore;
-import org.stormrealms.stormcore.util.RateLimiter;
-import org.stormrealms.stormstats.data.StatRepo;
-import org.stormrealms.stormstats.menus.CreateCharacterMenu;
-import org.stormrealms.stormstats.model.RPGPlayer;
 
 /**
  * 
@@ -26,28 +17,19 @@ import org.stormrealms.stormstats.model.RPGPlayer;
 @Component
 public class StatLoginListener implements Listener {
 	@Autowired
-	private AutowireCapableBeanFactory factory;
-	@Autowired
-	private StatRepo repo;
-	@Autowired
 	@Qualifier("needs-character")
 	private List<UUID> characterless;
+	/*
+	@Autowired
+	private StatRepo repo;
 
 	private RateLimiter limiter = new RateLimiter(TimeUnit.SECONDS, 10, (p) -> {
 		p.sendMessage("TEST");
-	});
+	});*/
 
 	@EventHandler
-	public void onChat(PlayerJoinEvent e) {
-		repo.getRecord(e.getPlayer().getUniqueId()).thenAccept((rp) -> {
-			RPGPlayer rPlayer = new RPGPlayer();
-			Bukkit.getScheduler().scheduleSyncDelayedTask(StormCore.getInstance(), () -> {
-				limiter.tryAction(e.getPlayer(), () -> {
-					CreateCharacterMenu charmenu = factory.getBean(CreateCharacterMenu.class);
-					// charmenu.open(e.getPlayer(), rPlayer);
-				}, () -> System.out.println("FAIL"));
-			});
-		});
+	public void onChat(PlayerInteractEvent e) {
+
 	}
 
 }
--- src/main/java/org/stormrealms/stormstats/model/RPGCharacter.java
+++ src/main/java/org/stormrealms/stormstats/model/RPGCharacter.java
@@ -55,6 +55,8 @@ public class RPGCharacter {
 	@Column
 	private int str;
 	@Column
+	private int sta;
+	@Column
 	private int intel;
 	@Column
 	private int spi;
