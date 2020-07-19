16ae1641555a0ef60000510cc39c80aeec28a509
James King <allthepie@gmail.com>
2020-07-19 12:06:04 -0400

    Fix copy pasta

--- StormNet/src/main/java/org/stormrealms/stormnet/configuration/StormNetConfiguration.java
+++ StormNet/src/main/java/org/stormrealms/stormnet/configuration/StormNetConfiguration.java
@@ -4,9 +4,5 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan({ "org.stormrealms.stormmenus", "org.stormrealms.stormmenus.abstraction",
-		"org.stormrealms.stormmenus.menus", "org.stormrealms.stormmenus.listeners",
-		"org.stormrealms.stormmenus.menus" })
-public class StormNetConfiguration {
-
-}
+@ComponentScan({ "org.stormrealms.stormnet", "org.stormrealms.stormnet.configuration" })
+public class StormNetConfiguration { }
