dc2077521d7067d3d55c9790845e1884c23115af
James King <allthepie@gmail.com>
2020-07-23 15:24:39 -0400

    Unnecessary config class

--- StormNet/src/main/java/org/stormrealms/stormnet/configuration/StormNetConfiguration.java
+++
@@ -1,8 +0,0 @@
-package org.stormrealms.stormnet.configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan({ "org.stormrealms.stormnet", "org.stormrealms.stormnet.configuration" })
-public class StormNetConfiguration { }
